18753260e7531d4b0ef98b0609418a0f3295fa5a
unknown <nrhawkins@gmail.com>
2016-02-23 20:53:59 +0000

    first commit

---
+++ src/main/java/com/quantumbits/Mandlebrot.java
@@ -0,0 +1,10 @@
+package com.quantumbits.fractals;
+
+public class Mandlebrot {
+
+    public static void main(String[] args) {
+        // Prints "Welcome to the Mandlebrot Set!" to the terminal window.
+        System.out.println("Welcome to the Mandlebrot Set!");
+    }
+
+}
---
+++ src/main/scala/com/quantumbits/fractals/Mandlebrot.java
@@ -0,0 +1,8 @@
+package com.quantumbits.fractals
+
+
+object Mandelbrot {
+  def main(args: Array[String]): Unit = {
+        println("Welcome to the Mandlebrot Set!")
+  }
+}
